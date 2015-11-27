a25285862d80a8dfd6d3ec1d650fe82ee865d240
Sakurai Masayoshi <7413045@ed.tus.ac.jp>
2015-11-27 17:35:50 +0900

    g

---
+++ api/model/enter.php
@@ -0,0 +1,22 @@
+<?php
+
+class enter extends Model
+{
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    //model
+    public function create()
+    {
+
+        $sql = "INSERT INTO $this->table (name,is_frend ) VALUES (:name, :is_friend)";
+
+
+        ]);
+
+
+
+
+    }
--- api/model/group.php
+++ api/model/group.php
@@ -4,12 +4,28 @@ class Group extends Model
 {
     function __construct()
     {
-        echo 'ok!';
+        parent::__construct();
     }
 
     //Controller
-    public function create()
+
+    public function room_create()
     {
+      $this- create();
+
+        ]);
+
+
+
+        public function friend_create()
+    {
+        $this- create();
+
+        ]);
+
+
+
+
     }
 
     public function find()
@@ -17,5 +33,21 @@ class Group extends Model
     }
 
     //Model
+
+    public function create()
+    {
+
+        $sql = "INSERT INTO $this->table (name,is_friend ) VALUES (:name, :is_friend)";
+        $stmt = $this->dbh->prepare($sql);
+        $this->res["db"] = $stmt->execute([
+            ':name' => $this->data["name"],
+            ':is_friend' => $this->data["is_friend"]
+
+        ]);
+
+
+
+
+    }
 }
 

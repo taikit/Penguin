e7c6f8b8309c6ace486f978b8d2464cde2329ab8
Sakurai Masayoshi <7413045@ed.tus.ac.jp>
2015-12-17 17:50:31 +0900

    頑張りました

--- api/model/message.php
+++ api/model/message.php
@@ -39,7 +39,7 @@ class Message extends Model
            if (isset($this->data["last_message_id"])) {
                ':last_message_id' => $this->data["last_message_id"],
                }
-            ':user_id' => $this->data["user_id"],
+            ':user_id' => $this->data["user_id"]
 
         ]);
         $this->res["data"] = $stmt->fetchAll(PDO::FETCH_ASSOC);

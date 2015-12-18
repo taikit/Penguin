9f0a7d753a2205e1d75fb3d0162d12b1de8a5279
Sakurai Masayoshi <7413045@ed.tus.ac.jp>
2015-12-18 17:18:36 +0900

    頑張りました

--- api/model/room.php
+++ api/model/room.php
@@ -83,7 +83,7 @@ order by message.time ASC limit 20";
                 ]);
 
                 $stmt->fetchAll(PDO::FETCH_ASSOC);
-                $array['room_name'] =  $this->res["db"]['name'];
+                $array['room_name'] =  $this->res["data"]['room_name']['name'] ;
 
 
             }

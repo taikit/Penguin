ea3fb126bf99eaffaf599033647a8059f5db8701
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-12-18 14:57:07 +0900

    fix post data format

--- api/model.php
+++ api/model.php
@@ -25,7 +25,7 @@ class Model
         $this->table = mb_strtolower(get_class($this));
 
         //dataの取得
-        $this->data = $_POST;
+        $this->data = json_decode($_POST['data'], true);
         if (!empty($_SESSION['user_id'])) {
             $this->data['user_id'] = $_SESSION['user_id'];
         }

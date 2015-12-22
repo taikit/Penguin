d10da84caa4bdc17ea5961a64e4b115b07caf82f
Taiki Tsunekawa <fantastic.ron@gmail.com>
2015-12-22 15:19:32 +0900

    fix for sp

--- js/bundle.js
+++ js/bundle.js
@@ -34629,8 +34629,8 @@ module.exports = Signup;
 var keyMirror = require('keymirror');
 var hostName = document.location.hostname;
 function get_endpoint() {
-    if (hostName == "localhost") {
-        return "http://localhost:8080/api/index.php";
+    if (hostName == "localhost" || hostName == "127.0.0.1") {
+        return "http://" + hostName + ":8080/api/index.php";
     } else {
         return "https://penguin-tus.azurewebsites.net//api/index.php";
     }
--- src/constants/Constants.js
+++ src/constants/Constants.js
@@ -1,8 +1,8 @@
 var keyMirror = require('keymirror');
 var hostName = document.location.hostname;
 function get_endpoint() {
-    if (hostName == "localhost") {
-        return "http://localhost:8080/api/index.php";
+    if (hostName == "localhost" || hostName == "127.0.0.1") {
+        return "http://" + hostName + ":8080/api/index.php";
     } else {
         return "https://penguin-tus.azurewebsites.net//api/index.php";
     }
